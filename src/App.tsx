52d4ef0061d8f9eae50de6c68bae9fe0104f18c8
xieyankun <kiroader@gmail.com>
2018-08-06 22:02:40 +0800

    feat: add marked

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,46 @@
+/* tslint-disable */
 import * as CodeMirror from 'codemirror';
+import * as Marked from 'marked'
 import * as React from 'react';
+
 import './App.css';
 
 import 'codemirror/lib/codemirror.css'
 
 import logo from './logo.svg';
 
-class App extends React.Component {
+interface IStete {
+  test: string,
+}
+
+class App extends React.Component<{}, IStete> {
+
+  private test: HTMLElement | null
+
+  constructor(props: Readonly<{}>) {
+    super(props)
+  }
 
   public componentDidMount() {
     const codemirror = document.querySelector('.codemirror');
-    window.console.log('CodeMirror', codemirror);
+
+    window.console.log('CodeMirror', Marked);
+
     const myCodeMirror = CodeMirror(codemirror as HTMLElement, {
       mode: 'javascript',
       value: '',
     });
 
-    window.console.log(myCodeMirror)
-
     myCodeMirror.on('change', (instance, changeObj) => {
       window.console.log(instance, changeObj)
-      
+      window.console.log(instance.getValue())
+      const value = instance.getValue()
+      // const content = document.querySelector('.content');
+
+      if (this.test != null) {
+        this.test.innerHTML = Marked(value);
+      }
+
     })
   }
 
@@ -35,6 +55,7 @@ class App extends React.Component {
           To get started, edit <code>src/App.tsx</code> and save to reload.
         </p>
         <div className="codemirror" />
+        <div className="content" ref={(com) => {this.test = com} } />
       </div>
     );
   }
