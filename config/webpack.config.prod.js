acd3f3e60ce9d40f5fd162a6213d7793d07cac5d
xieyankun <kiroader@gmail.com>
2018-08-09 21:31:50 +0800

    feat: add default

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -182,7 +182,7 @@ module.exports = {
           // use the "style" loader inside the async code so CSS from them won't be
           // in the main CSS file.
           {
-            test: /\.css$/,
+            test: /\.(css|scss|sass|less)$/,
             loader: ExtractTextPlugin.extract(
               Object.assign(
                 {
@@ -220,6 +220,9 @@ module.exports = {
                           }),
                         ],
                       },
+                    },
+                    {
+                      loader: require.resolve('sass-loader'),
                     },
                   ],
                 },
--- src/components/editor/index.tsx
+++ src/components/editor/index.tsx
@@ -4,6 +4,7 @@ import * as Marked from 'marked'
 import * as React from 'react';
 
 import 'codemirror/lib/codemirror.css'
+import 'github-markdown-css'
 
 import './main.scss'
 
@@ -21,27 +22,31 @@ class KillerMd extends React.Component<{}, IStete> {
 
   public componentDidMount() {
     const codemirror = document.querySelector('.codemirror');
+    const initCodeValue = '# 在此处输入标题\n\n标签（空格分隔）： `未分类`\n\n---\n\n在此输入正文';
 
     window.console.log('CodeMirror', Marked);
 
     const myCodeMirror = CodeMirror(codemirror as HTMLElement, {
-      lineNumbers: true,
       lineWrapping: true,   // 代码折叠
-      tabSize: 4,
-      value: ''
+      tabSize: 2,
+      value: initCodeValue
     });
+    
+    if (this.test != null) {
+      this.test.innerHTML = Marked(initCodeValue);
+    }
 
     myCodeMirror.on('change', (instance, changeObj) => {
       window.console.log(instance, changeObj)
       window.console.log(instance.getValue())
       const value = instance.getValue()
       // const content = document.querySelector('.content');
-
       if (this.test != null) {
         this.test.innerHTML = Marked(value);
       }
-
     })
+
+
   }
 
   public render() {
@@ -55,7 +60,7 @@ class KillerMd extends React.Component<{}, IStete> {
 
         <div className="md-editor">
           <div className="codemirror" />
-          <div className="content" ref={(com) => {this.test = com} } />
+          <div className="content markdown-body" ref={(com) => {this.test = com} } />
         </div>
       </div>
     );
